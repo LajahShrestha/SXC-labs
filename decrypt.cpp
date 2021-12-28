c7e5542a396ca23d2c119f923f0ac0d192c1e50f
LajahShrestha <51512893+LajahShrestha@users.noreply.github.com>
2021-12-28 10:49:56 +0545

    Add files via upload

---
+++ decrypt.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<string.h>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+	char PT[10] , CT[10];
+	cout<<"enter the message"<<endl;
+	gets(PT);
+	int key=3;
+	
+	//c=p+kmod10= Encryption
+	for(int i=0;PT[i]!='\0' ;i++)
+	{
+		char ch=PT[i];
+		if(ch>='a' && ch<= 'z')
+		{
+			ch=ch + key;
+			if(ch >'z')
+			{
+				ch = ch-'z' + 'a' -1;
+			}
+			
+			PT[i]=ch;
+		}
+	}
+	cout<<"the encrypted msg is: ";
+	for(int i=0;PT[i]!= '\0' ;i++)
+	{
+		cout<<PT[i];
+	}
+	
+	
+		//c=p-kmod10= Decryption
+	for(int i=0;PT[i]!='\0' ;i++)
+	{
+		char ch=PT[i];
+		if(ch>='a' && ch<= 'z')
+		{
+			ch=ch - key;
+			if(ch <'a')
+			{
+				ch = ch +'z' - 'a' +1;
+			}
+			
+			PT[i]=ch;
+		}
+	}
+	
+	cout<<"\nthe decrypted msg is: ";
+	for(int i=0;PT[i]!= '\0' ;i++)
+	{
+		cout<<PT[i];
+	}
+	return 0;
+}
